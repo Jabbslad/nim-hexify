31ffc67e7119c4262fb9ac6d63a64aa74ae20b5d
Jamie Atkinson <jabbslad@gmail.com>
2018-10-06 00:37:06 +0100

    fixed test naming

--- tests/test1.nim
+++ tests/test1.nim
@@ -2,14 +2,14 @@ import unittest
 
 import hexify
 
-test "0 should be 0":
+test "0 dec should be 0 hex":
   check "0" == hexify(0)
 
-test "201 as hex should be C9":
+test "201 dec should be C9 hex":
   check "C9" == hexify(201)
 
-test "39554 as hex should be 9A82":
+test "39554 dec should be 9A82 hex":
   check "9A82" == hexify(39554)
 
-  test "C9 as hex should be 201":
-    check unhexify("C9") == 201
+test "C9 hex should be 201 decimal":
+  check unhexify("C9") == 201
