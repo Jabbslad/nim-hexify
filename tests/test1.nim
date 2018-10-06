bc22c54389c39ea00f16e41f87942714935157d5
Jamie Atkinson <jabbslad@gmail.com>
2018-10-06 01:41:03 +0100

    added unhexify tests

--- tests/test1.nim
+++ tests/test1.nim
@@ -11,5 +11,11 @@ test "201 dec should be C9 hex":
 test "39554 dec should be 9A82 hex":
   check "9A82" == hexify(39554)
 
+test "0 hex should be 0 dec":
+  check 0 == unhexify("0")
+
 test "C9 hex should be 201 decimal":
   check unhexify("C9") == 201
+
+test "AB2 hex should be 2738 decimal":
+  check unhexify("AB2") == 2738
